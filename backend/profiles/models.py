684154251b6528abb2b8f3e82a1db6d39aa7eeb3
mor mbathie <mormbathie98@gmail.com>
2025-05-29 20:05:40 +0100

    monitorer le model

--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django_prometheus.models import ExportModelOperationsMixin
 
-class UserProfile(models.Model):
+
+class UserProfile(ExportModelOperationsMixin('userprofile'), models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
